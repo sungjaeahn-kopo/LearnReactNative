c4b649e97e4e0a71cf6fb918b976d54177ac46bd
LValue <dtw421@naver.com>
2022-12-31 22:29:49 +0900

    RN_ToDoApp initial commit

--- App.js
+++ App.js
@@ -6,33 +6,16 @@
  */
 
 import React, {useState} from 'react';
-import {SafeAreaView, Button, StyleSheet} from 'react-native';
-import Box from './components/Box';
-import Counter from './components/Counter';
+import {SafeAreaView, View, Text, Button, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [visible, setVisible] = useState(true);
-
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
-  const onPress = () => {
-    setVisible(!visible);
-  };
   return (
-    <SafeAreaView style={styles.full}>
-      <Button title="토글" onPress={onPress} />
-      {/* {visible && <Box rounded={true} size="large" color="blue" />} */}
-      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+    <SafeAreaView>
+      <View>
+        <Text>ToDoApp</Text>
+      </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  full: {
-    flex: 1,
-    // backgroundColor: 'cyan', // 전체영역 사용여부 확인용
-  },
-});
 
 export default App;
