784e59ae6957f48b3897d9727f6d21bc363be2cd
LValue <dtw421@naver.com>
2022-12-31 16:24:20 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Greeting from './components/Greeting';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Greeting />
+    </SafeAreaView>
+  );
+};
+
+export default App;
