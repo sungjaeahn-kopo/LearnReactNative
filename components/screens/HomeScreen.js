e3284474d44d384dff19782d5bbcbf8b65f5431b
sungjaeahn-kopo <dtw421@naver.com>
2023-01-18 23:40:37 +0900

    RN_Add Screen Components

---
+++ components/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, View} from 'react-native';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Button title="Detail 열기" />
+    </View>
+  );
+}
+
+export default HomeScreen;
