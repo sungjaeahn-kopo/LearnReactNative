0e8dbdaf33e8ff45acdc1f454618df1335b87066
sungjaeahn-kopo <dtw421@naver.com>
2023-01-19 01:06:57 +0900

    RN_Route Parameter

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -4,7 +4,18 @@ import {Button, View} from 'react-native';
 function HomeScreen({navigation}) {
   return (
     <View>
-      <Button title="Detail 열기" onPress={() => navigation.push('Detail')} />
+      <Button
+        title="Detail 1 열기"
+        onPress={() => navigation.push('Detail', {id: 1})}
+      />
+      <Button
+        title="Detail 2 열기"
+        onPress={() => navigation.push('Detail', {id: 2})}
+      />
+      <Button
+        title="Detail 3 열기"
+        onPress={() => navigation.push('Detail', {id: 3})}
+      />
     </View>
   );
 }
