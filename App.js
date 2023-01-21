97b39078d722b7ad6410652425d20194d028b182
sungjaeahn-kopo <dtw421@naver.com>
2023-01-22 01:44:31 +0900

    RN_Transform Stack to Drawer

--- App.js
+++ App.js
@@ -14,22 +14,41 @@ import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
 import TodoList from './components/TodoList';
 import todosStorage from './storages/todosStorage';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import HomeScreen from './components/screens/HomeScreen';
-import DetailScreen from './components/screens/DetailScreen';
-import HeaderlessScreen from './components/screens/HeaderlessScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title="Drawer 열기" onPress={() => navigation.openDrawer()} />
+      <Button
+        title="Setting 열기"
+        onPress={() => navigation.navigate('Setting')}
+      />
+    </View>
+  );
+}
+
+function SettingScreen({navigation}) {
+  return (
+    <View>
+      <Text>Setting</Text>
+      <Button title="뒤로가기" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
 
 const App = () => {
-  const today = new Date();
+  // const today = new Date();
 
-  const [todos, setTodos] = useState([
-    {id: 1, text: '작업환경 설정', done: true},
-    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
-    {id: 3, text: '투두리스트 만들어보기', done: false},
-  ]);
+  // const [todos, setTodos] = useState([
+  //   {id: 1, text: '작업환경 설정', done: true},
+  //   {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+  //   {id: 3, text: '투두리스트 만들어보기', done: false},
+  // ]);
 
   // Async Storage 불러오기
   // useEffect(() => {
@@ -42,29 +61,29 @@ const App = () => {
   //   todosStorage.set(todos).catch(console.error);
   // }, [todos]);
 
-  const onInsert = text => {
-    const nextId =
-      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
-    const todo = {
-      id: nextId,
-      text,
-      done: false,
-    };
+  // const onInsert = text => {
+  //   const nextId =
+  //     todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+  //   const todo = {
+  //     id: nextId,
+  //     text,
+  //     done: false,
+  //   };
 
-    setTodos(todos.concat(todo));
-  };
+  //   setTodos(todos.concat(todo));
+  // };
 
-  const onToggle = id => {
-    const nextTodos = todos.map(todo =>
-      todo.id === id ? {...todo, done: !todo.done} : todo,
-    );
-    setTodos(nextTodos);
-  };
+  // const onToggle = id => {
+  //   const nextTodos = todos.map(todo =>
+  //     todo.id === id ? {...todo, done: !todo.done} : todo,
+  //   );
+  //   setTodos(nextTodos);
+  // };
 
-  const onRemove = id => {
-    const nextTodos = todos.filter(todo => todo.id !== id);
-    setTodos(nextTodos);
-  };
+  // const onRemove = id => {
+  //   const nextTodos = todos.filter(todo => todo.id !== id);
+  //   setTodos(nextTodos);
+  // };
 
   return (
     // <SafeAreaProvider>
@@ -83,69 +102,38 @@ const App = () => {
     //   </SafeAreaView>
     // </SafeAreaProvider>
     <NavigationContainer>
-      <Stack.Navigator
+      <Drawer.Navigator
         initialRouteName="Home"
         // 모든 화면에서 Header 없애는 code
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen
+        screenOptions={{
+          drawerActiveBackgroundColor: '#fb8c00',
+          drawerActiveTintColor: 'white',
+        }}
+        drawerPosition="left"
+        backBehavior="history">
+        <Drawer.Screen
           name="Home"
           component={HomeScreen}
-          options={{
-            title: '홈',
-            headerStyle: {
-              backgroundColor: '#29b6f6',
-            },
-            headerTintColor: '#ffffff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 20,
-            },
-          }}
+          options={{title: '홈'}}
         />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{
-            // 안드로이드 백 버튼 감추기
-            headerBackVisible: false,
-            headerLeft: ({onPress}) => (
-              <TouchableOpacity onPress={onPress}>
-                <Text>Left</Text>
-              </TouchableOpacity>
-            ),
-            headerTitle: ({children}) => (
-              <View>
-                <Text>{children}</Text>
-              </View>
-            ),
-            headerRight: () => (
-              <View>
-                <Text>Right</Text>
-              </View>
-            ),
-          }}
-          // options={({route}) => ({
-          //   title: `상세 정보 - ${route.params.id}`,
-          // })}
+        <Drawer.Screen
+          name="Setting"
+          component={SettingScreen}
+          options={{title: '설정'}}
         />
-        <Stack.Screen
-          name="Headerless"
-          component={HeaderlessScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  block: {
-    flex: 1,
-    backgroundColor: 'white', // 안드로이드 기본 배경색은 회색
-  },
-  avoid: {
-    flex: 1, // ios는 keyboardAvoidingPlatform으로 키보드 동작 설정
-  },
-});
+// const styles = StyleSheet.create({
+//   block: {
+//     flex: 1,
+//     backgroundColor: 'white', // 안드로이드 기본 배경색은 회색
+//   },
+//   avoid: {
+//     flex: 1, // ios는 keyboardAvoidingPlatform으로 키보드 동작 설정
+//   },
+// });
 
 export default App;
